a8777415e9037f329b38a1ada7b2e47113a16785
robertocnws <robertoc@newwindowsystem.com>
2025-05-22 14:46:43 -0400

    Remove unused JavaScript files and MongoDB Docker configurations
    - Deleted load-ajax-form.js and prettify-min.js as they are no longer needed.
    - Removed commands.txt containing setup instructions for Django, Docker, and SSL.
    - Eliminated Dockerfile and initialization script for MongoDB.
    - Removed Dockerfile for mongo-express as it is not in use.
    - Updated celerybeat-schedule binary file.

--- api_ms_main_load_auth/app/ms_consume_loaded_zoho/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class MsConsumeLoadedZohoConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'ms_consume_loaded_zoho'
--- api_ms_main_load_auth/app/ms_load_from_zoho/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class MsLoadFromZohoConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'ms_load_from_zoho'
